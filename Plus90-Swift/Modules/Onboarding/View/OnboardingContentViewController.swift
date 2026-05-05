0e1ceb5cb11fea9e9f0f37f92e32fa79fc3e7b11
Mahmoud Mohamed <mahmoudbayoumi110@gmail.com>
2026-05-05 14:33:09 -0700

    add page viewcontroller and its content view controller

---
+++ Plus90-Swift/Modules/Onboarding/View/OnboardingContentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingContentViewController.swift
+//  Plus90-Swift
+//
+//  Created by Bayoumi on 05/05/2026.
+//
+
+import UIKit
+
+class OnboardingContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Plus90-Swift/Modules/Onboarding/View/OnboardingPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingPageViewController.swift
+//  Plus90-Swift
+//
+//  Created by Bayoumi on 05/05/2026.
+//
+
+import UIKit
+
+class OnboardingPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

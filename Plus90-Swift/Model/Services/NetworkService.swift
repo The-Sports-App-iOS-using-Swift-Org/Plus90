6b68d1549fe55da8b82b7864c8b0d8f4cd1d22b3
esraaehab333 <esraaehabb333@gmail.com>
2026-05-07 19:22:09 +0200

    refactor: edit the network service to using the alarmofire

--- Plus90-Swift/Model/Services/NetworkService.swift
+++ Plus90-Swift/Model/Services/NetworkService.swift
@@ -6,39 +6,25 @@
 //
 
 import Foundation
+import Alamofire
+
 protocol NetworkServiceProtocol{
     func fetchAllLeagues(completion:@escaping(LeagueResponse?)->Void)
 }
 
 class NetworkService: NetworkServiceProtocol {
     func fetchAllLeagues(completion: @escaping (LeagueResponse?) -> Void) {
-        let urlString = "https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=f53e0b0a6b8c53985ca5b20708c34bdb7ad3cff2465075297be0bada5f7b0983" // change the api
-        guard let url = URL(string: urlString) else {
-            print("Invalid URL")
-            completion(nil)
-            return
-        }
-        let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                print("Error fetching leagues: \(error.localizedDescription)")
-                completion(nil)
-                return
+        let urlString = "https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=YOUR_KEY"// this is my
+        AF.request(urlString)
+            .validate()
+            .responseDecodable(of: LeagueResponse.self) { response in
+                switch response.result {
+                case .success(let leaguesResult):
+                    completion(leaguesResult)
+                case .failure(let error):
+                    print("Request failed with error: \(error.localizedDescription)")
+                    completion(nil)
+                }
             }
-            guard let data = data else {
-                print("No data received")
-                completion(nil)
-                return
-            }
-            let decoder = JSONDecoder()
-            do {
-                let leaguesResult = try decoder.decode(LeagueResponse.self, from: data)
-                completion(leaguesResult)
-            } catch let error {
-                print("Decoding error: \(error)")
-                completion(nil)
-            }
-            
-        }.resume()
     }
 }

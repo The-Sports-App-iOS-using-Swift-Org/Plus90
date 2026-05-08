4c8ee7258b298b7eeecedf1f60affc80935d96cc
esraaehab333 <esraaehabb333@gmail.com>
2026-05-08 06:15:15 +0200

    feat: handle the sections of the collection view (Leagues Details) in storyboard

---
+++ Plus90-Swift/View/UpcomingCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingCollectionViewCell.swift
+//  Plus90-Swift
+//
+//  Created by Nemo on 08/05/2026.
+//
+
+import UIKit
+
+class UpcomingCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

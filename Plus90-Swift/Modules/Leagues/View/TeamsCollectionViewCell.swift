b14989be38feb8035a4e40efa4734d6a125d3b8c
Mahmoud Mohamed <mahmoudbayoumi110@gmail.com>
2026-05-09 07:53:01 -0700

    add teams collection view cell

---
+++ Plus90-Swift/Modules/Leagues/View/TeamsCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  TeamsCollectionViewCell.swift
+//  Plus90-Swift
+//
+//  Created by Bayoumi on 09/05/2026.
+//
+
+import UIKit
+
+class TeamsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var teamLogoImageView: UIImageView!
+    @IBOutlet weak var teamNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        teamLogoImageView.layer.cornerRadius = teamLogoImageView.frame.size.width / 2
+    }
+
+    private func setupUI() {
+        teamLogoImageView.contentMode = .scaleAspectFill
+        teamLogoImageView.clipsToBounds = true
+        
+        teamLogoImageView.layer.borderWidth = 2.0
+        teamLogoImageView.layer.borderColor = UIColor.systemGray5.cgColor
+    }
+
+    func configure(with team: Team) {
+        teamNameLabel.text = team.teamName
+
+        if let logoUrl = team.teamLogo {
+            teamLogoImageView.loadImage(from: logoUrl, placeholder: UIImage(named: "placeholder"))
+        }
+    }
+}
